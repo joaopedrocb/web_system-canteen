175b83c760967e664cc5d97c75e65b1ef8ab34eb
Júlio Mussi <jc.mussi99@gmail.com>
2021-09-21 20:45:37 -0300

    feat(data): creates mocks

---
+++ unifacs-food-club/src/data/purchaseHistoric.js
@@ -0,0 +1,37 @@
+export const purchaseHistoric = [
+  {
+    date: '21/09/2021',
+    productName: 'Bauru',
+    value: 5,
+  },
+  {
+    date: '21/08/2021',
+    productName: 'Dino burguer',
+    value: 12,
+  },
+  {
+    date: '10/08/2021',
+    productName: 'Dinosoba',
+    value: 25,
+  },
+  {
+    date: '09/08/2021',
+    productName: 'Tapioca do Dino',
+    value: 8,
+  },
+  {
+    date: '09/08/2021',
+    productName: 'Coca-cola',
+    value: 4,
+  },
+  {
+    date: '21/09/2020',
+    productName: 'Bauru',
+    value: 5,
+  },
+  {
+    date: '21/09/2020',
+    productName: 'Agua de coco',
+    value: 2,
+  },
+]
