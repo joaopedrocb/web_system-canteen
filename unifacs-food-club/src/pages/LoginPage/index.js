4b025c382c6e1882262996d437094f67e058ff73
Júlio Mussi <jc.mussi99@gmail.com>
2021-09-21 22:57:41 -0300

    feat(LoginPage): implements accessLevel map

--- unifacs-food-club/src/pages/LoginPage/index.js
+++ unifacs-food-club/src/pages/LoginPage/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import { default as LoginPagePresentation } from './presentation';
+import { Responsible, Staff, Student } from '../../models'
+import { LocalStorageAdapter } from '../../infra'
+import { LOGGED_USER } from '../../constants'
 
 export function LoginPage() {
+
+  const storage = new LocalStorageAdapter();
+  
+  const responsible = new Responsible()
+  
+  const student = new Student();
+  
+  const staff = new Staff()
+
+  const validResponsibleEmail = responsible.email;
+
+  const validStudentEmail = student.email;
+
+  const validStaffEmail = staff.email;
+
   const [email, setEmail] = React.useState('');
 
   function onEmailInputChange(event) {
@@ -14,9 +32,97 @@ export function LoginPage() {
     setPassword(event?.target?.value);
   }
 
-  function onClickLogin(email, password) {
-    console.log(email, password)
+  function saveResponsible() {
+    const { 
+      cpf, 
+      name, 
+      phoneNumber,
+      email,
+      login, 
+      password,
+      studentsEnrollment,
+      accessLevel,
+     } = responsible;
+  
+    storage.setItem(LOGGED_USER, { 
+      cpf, 
+      name, 
+      phoneNumber,
+      email,
+      login, 
+      password,
+      studentsEnrollment,
+      accessLevel,
+    });
   }
+  
+  
+  function saveStudent() {
+    const {
+      studentEnrollment,
+      studentClass,
+      shift,
+      name,
+      phoneNumber,
+      email,
+      login,
+      password,
+      responsibleCpf,
+      balance,
+      accessLevel,
+    } = student;
+  
+    storage.setItem(LOGGED_USER, {
+      studentEnrollment,
+      studentClass,
+      shift,
+      name,
+      phoneNumber,
+      email,
+      login,
+      password,
+      responsibleCpf,
+      balance,
+      accessLevel,
+    });
+  }
+  
+  function saveStaff() {
+    const {
+      name,
+      adress,
+      phoneNumber,
+      email,
+      accessLevel,
+    } = staff;
+  
+    storage.setItem(LOGGED_USER, {   
+      name,
+      adress,
+      phoneNumber,
+      email,
+      accessLevel
+    });
+  }
+
+function onClickLogin() {
+
+  if (email === validResponsibleEmail && password === '12313') {
+    console.log('Logado como responsável:', responsible)
+    saveResponsible()
+  }
+
+  if (email === validStudentEmail && password === '12313') {
+    console.log('Logado como aluno:', student)
+    saveStudent()
+  }
+  
+
+  if (email === validStaffEmail && password === '12313') {
+    console.log('Logado como funcionário')
+    saveStaff()
+  }
+}
   
   return React.createElement(LoginPagePresentation, { 
     onClickLogin,
