aaf6e8c63bbf0e79926130e8e488efb8d0dc08cd
joao pedro <joaopedro@kinvo.com.br>
2021-09-21 21:51:00 -0300

    feat: Create layout of LoginPage

--- unifacs-food-club/src/pages/LoginPage/presentation.jsx
+++ unifacs-food-club/src/pages/LoginPage/presentation.jsx
@@ -1,9 +1,39 @@
 // dependencies
 import React from 'react';
+import { Link } from 'react-router-dom'
+
+// css
+import './styles.css';
 
 function LoginPagePresentation() {
     return (
-        <div>Login Page</div>
+        <main id="login-page">
+            <section class="login-page_section">
+                <span class="login-page_section-title">Entrar</span>
+
+                <form class="login-page_section-form">
+                    <input placeholder="Email"></input>
+                    <input placeholder="Senha"></input>
+                </form>
+
+                <footer class="login-page_section-footer">
+                    <Link to="/login" class="login-page_section-footer-forgot-password">
+                        Esqueceu a senha?
+                    </Link>
+
+                    <button>Entrar</button>
+
+                    <hr />
+
+                    <span class="login-page_section-footer-sign-up-text">
+                        Sua escola ainda não está cadastrada?
+                        {' '}
+                        <Link to="/login">Cadastre agora</Link>
+                    </span>
+
+                </footer>
+            </section>
+        </main>
     )
 }
 
