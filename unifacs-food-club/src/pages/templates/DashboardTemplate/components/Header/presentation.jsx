ca1bcf8231725895338df4b55eacfbf8078b13d6
Júlio Mussi <jc.mussi99@gmail.com>
2021-09-23 18:56:53 -0300

    feat: removes menagement button if no user is logged

--- unifacs-food-club/src/pages/templates/DashboardTemplate/components/Header/presentation.jsx
+++ unifacs-food-club/src/pages/templates/DashboardTemplate/components/Header/presentation.jsx
@@ -1,50 +1,68 @@
 // dependencies
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 // css
-import './styles.css';
+import "./styles.css";
 
 function HeaderPresentation(props) {
-    const { hasALoggedUser, managementPageHasBeenActive, clearSession } = props;
+  const { hasALoggedUser, managementPageHasBeenActive, clearSession } = props;
 
-    function renderLoginOrLogoutButton() {
-        if (hasALoggedUser) {
-            return <button onClick={clearSession} class="header_logout-button">Sair</button>
-        }
+  function renderLoginOrLogoutButton() {
+    if (hasALoggedUser) {
+      return (
+        <button onClick={clearSession} class="header_logout-button">
+          Sair
+        </button>
+      );
+    }
 
-        return (
-            <Link replace to="/login">
-                <button class="header_login-button">Login</button>
+    return (
+      <Link replace to="/login">
+        <button class="header_login-button">Login</button>
+      </Link>
+    );
+  }
+
+  function renderManagementButton() {
+    if (!hasALoggedUser) {
+      return null;
+    }
+
+    return (
+      <li class="header_navigation-options">
+        <Link replace to="/gerenciamento">
+          Gerenciamento
+        </Link>
+      </li>
+    );
+  }
+
+  function renderHeader() {
+    if (managementPageHasBeenActive) {
+      return null;
+    }
+
+    return (
+      <header id="dashboard_header">
+        <div class="dashboard_logo">{"."}</div>
+
+        <ul class="header_navigation">
+          <li class="header_navigation-options">
+            <Link replace to="/">
+              Início
             </Link>
-        );
-    }
+          </li>
 
-    function renderHeader() {
-        if (managementPageHasBeenActive) {
-            return null;
-        }
+          {renderManagementButton()}
+        </ul>
 
-        return (
-            <header id="dashboard_header">
-                <div class="dashboard_logo">{'.'}</div>
+        {renderLoginOrLogoutButton()}
+      </header>
+    );
+  }
 
-                <ul class="header_navigation">
-                    <li class="header_navigation-options">
-                        <Link replace to="/">Início</Link>
-                    </li>
-
-                    <li class="header_navigation-options">
-                        <Link replace to="/gerenciamento">Gerenciamento</Link>
-                    </li>
-                </ul>
-
-                {renderLoginOrLogoutButton()}
-            </header>
-        )
-    }
-
-    return renderHeader();
+  return renderHeader();
 }
 
 export default HeaderPresentation;
