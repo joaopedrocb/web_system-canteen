1524eb5d1c09eb0622d132b3a2f61f3adb824ddf
Júlio Mussi <jc.mussi99@gmail.com>
2021-09-29 22:46:15 -0300

    clean: app.js

--- unifacs-food-club/src/app/App.js
+++ unifacs-food-club/src/app/App.js
@@ -1,12 +1,16 @@
 // dependencies
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // css
 import "../global.css";
 
 // constants
-import { productsList as productsListFromDomain, responsiblesList as responsiblesListFromDomain, studentsList as studentsListFromDomain } from '../common/domain';
+import {
+  productsList as productsListFromDomain,
+  responsiblesList as responsiblesListFromDomain,
+  studentsList as studentsListFromDomain,
+} from "../common/domain";
 
 // pages
 import {
@@ -17,132 +21,158 @@ import {
   ResponsiblesPage,
   StudentsPage,
   ProductsPurchasePage,
-  UpdateProduct,
-  InsertStudent,
-  UpdateResponsible,
-  UpdateStudent,
   StatementPage,
   DepositsPage,
-  ManagementTemplate
-} from '../pages';
+  ManagementTemplate,
+} from "../pages";
 
 // enums
-import { AccessLevelEnum } from '../common/domain';
+import { AccessLevelEnum } from "../common/domain";
 
 function App() {
-
   const [userData, setUserData] = React.useState({
-    name: 'Unifacs',
-    adress : 'Av. Juracy Magalhães Júnior, S/N - Rio Vermelho, Salvador - BA, 41940-060',
-    phoneNumber : '(71) 3021-2800',
-    email : 'funcionario@gmail.com',
-    accessLevel : AccessLevelEnum.STAFF,
+    name: "Unifacs",
+    adress:
+      "Av. Juracy Magalhães Júnior, S/N - Rio Vermelho, Salvador - BA, 41940-060",
+    phoneNumber: "(71) 3021-2800",
+    email: "funcionario@gmail.com",
+    accessLevel: AccessLevelEnum.STAFF,
   });
 
-  console.log('userData', userData);
+  console.log("userData", userData);
 
-  const [productsList, setProductsList] = React.useState(productsListFromDomain);
+  const [productsList, setProductsList] = React.useState(
+    productsListFromDomain
+  );
 
-  console.log('productsList', productsList);
+  console.log("productsList", productsList);
 
-  const [responsiblesList, setResponsiblesList] = React.useState(responsiblesListFromDomain);
+  const [responsiblesList, setResponsiblesList] = React.useState(
+    responsiblesListFromDomain
+  );
 
-  console.log('responsiblesList', responsiblesList);
+  console.log("responsiblesList", responsiblesList);
 
-  const [studentsList, setStudentsList] = React.useState(studentsListFromDomain);
+  const [studentsList, setStudentsList] = React.useState(
+    studentsListFromDomain
+  );
 
-  console.log('studentsList', studentsList);
+  console.log("studentsList", studentsList);
 
   return (
     <div className="App">
       <Router>
         <DashboardTemplate>
           <Switch>
-            <Route path="/" exact component={() => <LandingPage userData={userData} />} />
+            <Route
+              path="/"
+              exact
+              component={() => <LandingPage userData={userData} />}
+            />
 
-            <Route path="/login" component={() => <LoginPage userData={userData} changeUserData={setUserData} />} />
+            <Route
+              path="/login"
+              component={() => (
+                <LoginPage userData={userData} changeUserData={setUserData} />
+              )}
+            />
 
-            <Route path="/gerenciamento" exact component={() => (
-              <ManagementTemplate userData={userData}>
-                <ProductsPage 
-                  productsList={productsList} 
-                  userData={userData} 
-                  changeProductsList={setProductsList}
-                />
-              </ManagementTemplate>
-            )} />
+            <Route
+              path="/gerenciamento"
+              exact
+              component={() => (
+                <ManagementTemplate userData={userData}>
+                  <ProductsPage
+                    productsList={productsList}
+                    userData={userData}
+                    changeProductsList={setProductsList}
+                  />
+                </ManagementTemplate>
+              )}
+            />
 
-            <Route path="/gerenciamento/produtos" exact component={() => (
-              <ManagementTemplate userData={userData}>
-                <ProductsPage 
-                  productsList={productsList} 
-                  userData={userData} 
-                  changeProductsList={setProductsList}
-                />
-              </ManagementTemplate>
-            )} />
+            <Route
+              path="/gerenciamento/produtos"
+              exact
+              component={() => (
+                <ManagementTemplate userData={userData}>
+                  <ProductsPage
+                    productsList={productsList}
+                    userData={userData}
+                    changeProductsList={setProductsList}
+                  />
+                </ManagementTemplate>
+              )}
+            />
 
-            <Route path="/comprar" exact component={() => (
-              <ManagementTemplate userData={userData}>
-                <ProductsPurchasePage 
-                  productsList={productsList} 
-                  userData={userData} 
-                />
-              </ManagementTemplate>
-            )} />
+            <Route
+              path="/comprar"
+              exact
+              component={() => (
+                <ManagementTemplate userData={userData}>
+                  <ProductsPurchasePage
+                    productsList={productsList}
+                    userData={userData}
+                  />
+                </ManagementTemplate>
+              )}
+            />
 
-            <Route path="/gerenciamento/responsaveis" exact component={() => (
-              <ManagementTemplate userData={userData}>
-                <ResponsiblesPage 
-                  responsiblesList={responsiblesList} 
-                  userData={userData} 
-                  updateResponsibles={setResponsiblesList}
-                />
-              </ManagementTemplate>
-            )} />
+            <Route
+              path="/gerenciamento/responsaveis"
+              exact
+              component={() => (
+                <ManagementTemplate userData={userData}>
+                  <ResponsiblesPage
+                    responsiblesList={responsiblesList}
+                    userData={userData}
+                    updateResponsibles={setResponsiblesList}
+                  />
+                </ManagementTemplate>
+              )}
+            />
 
-            <Route path="/gerenciamento/alunos" exact component={() => (
-              <ManagementTemplate userData={userData}>
-                <StudentsPage 
-                  studentsList={studentsList} 
-                  userData={userData} 
-                  changeStudentsList={setStudentsList}
-                />
-              </ManagementTemplate>
-            )} />
-      
-            <Route path="/gerenciamento/extrato" exact component={() => (
-              <ManagementTemplate userData={userData}>
-                <StatementPage />
-              </ManagementTemplate>
-            )} />
+            <Route
+              path="/gerenciamento/alunos"
+              exact
+              component={() => (
+                <ManagementTemplate userData={userData}>
+                  <StudentsPage
+                    studentsList={studentsList}
+                    userData={userData}
+                    changeStudentsList={setStudentsList}
+                  />
+                </ManagementTemplate>
+              )}
+            />
 
-            <Route path="/gerenciamento/depositos" exact component={() => (
-              <ManagementTemplate userData={userData}>
-                <DepositsPage />
-              </ManagementTemplate>
-            )} />
+            <Route
+              path="/gerenciamento/extrato"
+              exact
+              component={() => (
+                <ManagementTemplate userData={userData}>
+                  <StatementPage />
+                </ManagementTemplate>
+              )}
+            />
 
+            <Route
+              path="/gerenciamento/depositos"
+              exact
+              component={() => (
+                <ManagementTemplate userData={userData}>
+                  <DepositsPage />
+                </ManagementTemplate>
+              )}
+            />
 
-
-
-
-            {/* <Route path="/gerenciamento/produtos/adicionar" component={InsertProduct}/>
-
+            {/* 
+            
             <Route path="/gerenciamento/produtos/alterar" component={UpdateProduct}/>
-
-            <Route path="/gerenciamento/alunos/adicionar" component={InsertStudent}/>
-
-            <Route path="/gerenciamento/responsaveis/alterar" component={UpdateResponsible}/>
 
             <Route path="/gerenciamento/alunos/alterar" component={UpdateStudent}/>
 
-
-
-          
-
-
-            <Route path="/comprar" component={ProductsPurchasePage} /> */}
+            */}
           </Switch>
         </DashboardTemplate>
       </Router>
